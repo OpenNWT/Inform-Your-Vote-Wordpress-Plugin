6e35644c6b3d3c28d2f862965b8b01c7ea7747c9
Joel <joel.hurtig@aceproject.space>
2018-02-13 14:05:29 -0600

    Updated Mock Page and added Footer Nav.
    Note that the header changes is just whitespace, probably due to trying to merge with Heng's earlier work.

--- theme/page-mock-election.php
+++ theme/page-mock-election.php
@@ -16,8 +16,8 @@ get_header(); ?>
   <div id="content" class="site-content" role="main">
 
     <?php foreach ( $constituency['children'] as $name => $child ) : ?>
-      <?php $child_constituency = get_constituency($child['id']);
-      if($child_constituency['children']):?>
+      <?php $child_constituency = get_constituency($child[ 'id' ]);
+      if ($child_constituency[ 'children' ] ) :?>
       <h3><?php echo $name; ?></h3>
       <ul>
         <?php foreach ( $child_constituency['children'] as $g_name => $g_child) :?>
@@ -26,9 +26,10 @@ get_header(); ?>
           //store arrays and count
           $can_array = array();
           $sort_vote = array();
+          //$total_votes = array();
           $num_votes = 0;
-
           global $ed_post_types;
+
           $query_args = array(
             'post_type' => $ed_post_types['candidate'],
             'order' => 'ASC',
@@ -42,41 +43,54 @@ get_header(); ?>
             $candidates[$query->post->ID] = get_candidate( $query->post->ID, true );
           }
 
-          foreach($candidates as $can){
+          foreach( $candidates as $can ){
             $can_array[] = array('candidate_votes' => $can['candidate_votes'], 'name' => $can['name'], 'id' => $can['id']);
             $num_votes += $can['candidate_votes'];
           }
 
-          foreach($can_array as $v=>$key) {
+          foreach( $can_array as $v=>$key ) {
             $sort_vote[] = $key['candidate_votes'];
           }
-          array_multisort($sort_vote, SORT_DESC, $can_array);
-
+          array_multisort( $sort_vote, SORT_DESC, $can_array );
           ?>
           <table>
-            <tr> <th>Candidate</th> <th>Party</th> <th>Votes</th> <th>Percentage</th> </tr>
+            <tr> <th>Candidate</th>
+              <?php if (Election_Data_Option::get_option('party_election') ): ?> <th>Party</th> <?php endif ?>
+              <th>Votes</th>
+              <th>Percentage</th>
+            </tr>
             <?php
             foreach($can_array as $r=>$result) :
-              $can_party = get_party_from_candidate($result['id']); ?>
-
-              <tr style="color:<?php echo $can_party['colour'] ?>;"><td><?php echo $result['name']; ?></td>
-                <td><?php
-                echo $can_party['name']; ?></td>
+              $can_party = get_party_from_candidate( $result['id'] ); ?>
+              <tr style="color:<?php echo $can_party['colour'] ?>;">
+                <td><?php echo $result['name']; ?></td>
+                <?php if ( Election_Data_Option::get_option('party_election') ):
+                   $total_votes[$can_party['name']] += $result['candidate_votes']; ?>
+                  <td><?php echo $can_party['name']; ?></td>
+                <?php endif; ?>
                 <td><?php echo $result['candidate_votes']; ?></td>
                 <td><?php if ($result['candidate_votes']>0) {
                   echo round( ($result['candidate_votes'] / $num_votes), 3) * 100 . '%';
-                }	?></td></tr> <?php
-              endforeach; ?> </table> <br />
-              <?php
-            endforeach; ?>
-          </ul>
-        <?php else:?>
-          <li><a href="<?php echo $child['url']; ?>"><?php echo $name; ?></a></li>
-        <?php endif;?>
-      <?php endforeach; ?>
+                }	?></td>
+              </tr>
+            <?php endforeach; ?>
+          </table>
+          <p>Number of votes: <?php echo $num_votes ?> </p> <br />
+        <?php endforeach; ?>
+      </ul>
+    <?php else:?>
+      <li><a href="<?php echo $child['url']; ?>"><?php echo $name; ?></a></li>
+    <?php endif;?>
+  <?php endforeach; ?>
 
-    </div><!-- #content .site-content -->
-  </section><!-- #primary .content-area -->
-  <?php
-  get_footer();
-  ?>
+  <?php if (Election_Data_Option::get_option('party_election') ) {
+			foreach ($total_votes as $party_name => $party_total) {
+				echo '<p>Those under the ' . $party_name . ' banner received ' . $party_total . ' votes this election.';
+			}
+		}?>
+
+</div><!-- #content .site-content -->
+</section><!-- #primary .content-area -->
+<?php
+get_footer();
+?>
