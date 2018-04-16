2d9c1871d689f786eec2e310efefdb30b1e093cf
Sim <simranjeet.hunjan@aceproject.space>
2018-04-16 10:37:23 -0500

    Merge branch 'Address-Lookup-Tool-Fixes' into dev-branch

--- theme/ElectionData-V2/js/address-lookup.js
+++ theme/ElectionData-V2/js/address-lookup.js
@@ -1,23 +1,32 @@
 jQuery(document).ready( function($) {
 
   $('#address_lookup_form').submit(function(){
-    $('#candidates').css('display', 'none');
-    $('.search_candidates_text').css('display', 'none');
-    
-    $('.loading').css('display', 'block');
-    var form_data = $(this).serializeArray();
 
-    $.ajax({
-      url: ajaxurl,
-      type: "POST",
-      data: {form_data: form_data, action : 'address_lookup'},
-      success: function(data){
-        $('#candidates').css('display', 'block');
-        $('.loading').css('display', 'none');
-        $('#candidates').html(data);
-      }
-    });
+    var street_number = $('#street_number').val();
+    var street_name = $('#street_name').val();
+    var street_address = street_number + street_name;
 
+    if(street_address){
+      $('#candidates').css('display', 'none');
+      $('.search_candidates_text').css('display', 'none');
+
+      $('.loading').css('display', 'block');
+      var form_data = $(this).serializeArray();
+
+      $.ajax({
+        url: ajaxurl,
+        type: "POST",
+        data: {form_data: form_data, action : 'address_lookup'},
+        success: function(data){
+          $('#candidates').css('display', 'block');
+          $('.loading').css('display', 'none');
+          $('#candidates').html(data);
+        }
+      });
+    }
+    else{
+      alert("Please enter an address");
+    }
     return false;
   });
 
