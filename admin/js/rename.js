2366f5a7162f5f7a2332efe5ac231a3e76c7bf82
Joel <joel.hurtig@aceproject.space>
2018-04-24 09:23:34 -0500

    Corrected scripting
    Rewrites now work as intended.

--- admin/js/rename.js
+++ admin/js/rename.js
@@ -1,9 +1,8 @@
 /**
-*	Javascript for setting an explanation of what MetaData is when in the admin panel.
-*
+*	Javascript for setting the Answers menu item into something more descriptive
+* @since Election_Data 1.1
 */
 window.onload = function() {
-	alert ('this is a test!');
 	// Change the Answer item to something more descriptive via javascript
 		var answers = document.querySelector("#menu-posts-ed_answers > a > div.wp-menu-name");
 		if (answers != null) {
