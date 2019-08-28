b7530332a184f66fe5c5f334d401e775858039ac
Kyle Geske <stungeye@gmail.com>
2019-08-28 04:26:17 +0000

    Changed the grid size for party logos on the homepage as a new party was registered.

--- theme/ElectionData-V2/functions.php
+++ theme/ElectionData-V2/functions.php
@@ -48,7 +48,7 @@ function election_data_theme_scripts() {
   wp_enqueue_script( 'shuffle', get_template_directory_uri() . '/js/shuffle.js', array(), '1.0.3');
 //  wp_enqueue_script( 'address_lookup_js', get_template_directory_uri() . '/js/address-lookup.js', array(), '1.1.0' );
 
-  wp_enqueue_style( 'style', get_stylesheet_uri(), array(), '5.3.13');
+  wp_enqueue_style( 'style', get_stylesheet_uri(), array(), '5.3.14');
   // $updated_at = filemtime('wp-content/plugins/ElectionData/theme/ElectionData-V2/style.css');
   // wp_enqueue_style( 'style', get_stylesheet_uri(), array(), $updated_at);
 
